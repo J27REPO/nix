b60f7b8e12e8d12c0f17defc8eda1c4747ac5574
J27REPO <josesf2004@gmail.com>
2026-01-28 08:53:26 +0000

    sincronizacion

--- hosts/macmini/default.nix
+++ hosts/macmini/default.nix
@@ -6,60 +6,68 @@
   boot.kernelParams = [ "snd_hda_intel.model=apple-headset-multi" ];
   boot.kernelModules = [ "uinput" ];
 
+  # Regla uinput corregida para que Sunshine tenga permisos totales de entrada
   services.udev.extraRules = ''
-      KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess", GROUP="input", MODE="0660"
-    '';
-    
-  # 1. Bootloader
+    KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess", MODE="0660", GROUP="uinput"
+  '';
+
+  # 1. Bootloader y Wake on LAN
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  networking.interfaces.enp1s0.wakeOnLan = {
-      enable = true;
-      policy = [ "magic" ];
-    };
   
-    # 2. Instalar ethtool para comprobar que todo está bien
-    environment.systemPackages = [ pkgs.ethtool ];
-  # 2. Drivers Gráficos Intel (Específico Mac Mini)
+
+networking.interfaces.enx10ddb1c93253.wakeOnLan = {
+    enable = true;
+    policy = [ "magic" ];
+  };
+  # 2. Drivers Gráficos e Intel Packages
   services.xserver.videoDrivers = [ "intel" ]; 
   hardware.graphics.enable = true;
+  environment.systemPackages = [ 
+    pkgs.ethtool 
+    pkgs.vaapiIntel # Driver para que Sunshine codifique video por hardware
+  ];
 
-  # --- NUEVA CONFIGURACIÓN PARA ACCESO REMOTO ---
+  # --- ACCESO REMOTO ---
 
-  # 3. Habilitar SSH
+  # 3. SSH
   services.openssh = {
     enable = true;
     settings = {
-      PasswordAuthentication = true; # Permite entrar con tu contraseña
-      PermitRootLogin = "no";        # Seguridad: no permite entrar como root
+      PasswordAuthentication = true;
+      PermitRootLogin = "no";
     };
   };
 
-  # 4. Habilitar Tailscale
+  # 4. Tailscale y DNS
   services.tailscale.enable = true;
   networking.hostName = "macmini";
   services.resolved.enable = true;
-  # 5. Configurar Firewall para Tailscale y SSH
+
+  # 5. Sunshine (Streaming)
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+  };
+
+  # 6. Firewall manual (Refuerzo para Sunshine y SSH)
   networking.firewall = {
     enable = true;
-    # "loose" es necesario para que Tailscale atraviese el firewall correctamente
-    checkReversePath = "loose"; 
-    allowedUDPPorts = [ 41641 ]; # Puerto oficial de Tailscale
-    allowedTCPPorts = [ 22 ];    # Puerto estándar de SSH
+    checkReversePath = "loose";
+    allowedTCPPorts = [ 22 47984 47989 47990 48010 ];
+    allowedUDPPorts = [ 41641 47998 47999 48000 48002 48010 ];
   };
-  services.sunshine = {
-      enable = true;
-      autoStart = true;
-      capSysAdmin = true; # Necesario para crear dispositivos de entrada virtuales
-      openFirewall = true; # Abre los puertos 47984-48010
-    };
 
-  # ----------------------------------------------
+  # --- USUARIO Y GRUPOS ---
+  
+  # Creamos el grupo uinput para la regla udev
+  users.groups.uinput = {};
 
-  # 6. Definición de Usuario
   users.users.${user} = {
     isNormalUser = true;
     shell = pkgs.zsh;
-    extraGroups = [ "networkmanager" "wheel" "video" "docker" ]; # He añadido docker por si lo usas
+    extraGroups = [ "networkmanager" "wheel" "video" "docker" "uinput" "input" ];
   };
 }
